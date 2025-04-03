6cf6d2a22bd0461e034d0317a61f406553f6c24d
gpettro <gabrielruotolo@discente.ufg.br>
2025-04-03 15:58:22 -0300

    Update of the setup: Removing models of wakeword (already inside the container)

--- data_downloads/setup_openwakeword_resources.sh
+++ data_downloads/setup_openwakeword_resources.sh
@@ -3,16 +3,6 @@
 # Update and intall wget inside the docker container if not installed
 apt-get update && apt-get install -y wget
 
-# Create the directory structure for the models
-mkdir -p /opt/conda/lib/python3.10/site-packages/openwakeword/resources/models/
-
-# Install melspectrogram.onnx model and 
-wget https://github.com/dscripka/openWakeWord/releases/download/v0.5.1/melspectrogram.onnx -O /opt/conda/lib/python3.10/site-packages/openwakeword/resources/models/melspectrogram.onnx
-wget https://github.com/dscripka/openWakeWord/releases/download/v0.5.1/embedding_model.onnx -O /opt/conda/lib/python3.10/site-packages/openwakeword/resources/models/embedding_model.onnx
-wget https://github.com/dscripka/openWakeWord/releases/download/v0.5.1/melspectrogram.onnx -O /opt/conda/lib/python3.10/site-packages/openwakeword/resources/models/melspectrogram.tflite
-wget https://github.com/dscripka/openWakeWord/releases/download/v0.5.1/embedding_model.onnx -O /opt/conda/lib/python3.10/site-packages/openwakeword/resources/models/embedding_model.tflite
-
-
 # training set (~2,000 hours from the ACAV100M Dataset)
 # See https://huggingface.co/datasets/davidscripka/openwakeword_features for more information
 wget https://huggingface.co/datasets/davidscripka/openwakeword_features/resolve/main/openwakeword_features_ACAV100M_2000_hrs_16bit.npy
